461b14774e47ae3550e6a44a85259597adfd3f72
rixo <rixo@rixo.fr>
2020-04-15 00:40:15 +0200

    add zoarrc

---
+++ .zoarrc.js
@@ -0,0 +1,15 @@
+export default {
+  // pattern(s) to find test files
+  files: '**/*.spec.js',
+
+  // patterns of files to watch
+  watch: ['src/**/*', 'test/**/*'],
+
+  // patterns to always ignore when searching for files or watching
+  ignore: ['**/node_modules', '**/.git'],
+
+  // default pipes (--pipe argument overrides those, --no-pipes to disable)
+  // 'pipe.run': '',
+  // 'pipe.ls': '',
+  // 'pipe.print': '',
+}
