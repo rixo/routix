4dd9df0717a6c133e40210755a00ae8d20cde62d
rixo <rixo@rixo.fr>
2020-04-21 00:21:32 +0200

    nicely format custom props

--- src/build/routes.js
+++ src/build/routes.js
@@ -6,7 +6,12 @@ import { indent, _ref } from './util'
 const _ = JSON.stringify
 
 const _props = (props = {}) =>
-  Object.entries(props).map(([prop, value]) => `${_(prop)}: ${_(value)}`)
+  // Object.entries(props).map(([prop, value]) => `${_(prop)}: ${_(value)}`)
+  Object.entries(props).flatMap(([prop, value]) => {
+    const lines = JSON.stringify(value, false, 2).split('\n')
+    const first = lines.shift()
+    return [`${_(prop)}: ${first}`, ...lines.map(x => '    ' + x)].join('\n')
+  })
 
 const _children = children =>
   // NOTE children not here when tree:false
